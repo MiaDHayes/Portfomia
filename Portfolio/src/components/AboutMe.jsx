2d680679162ed239bc5e0f76afa4a8ef51c81973
MiaDHayes <miadeyon@outlook.com>
2024-03-17 21:21:33 -0500

    added image to about me

--- Portfolio/src/components/AboutMe.jsx
+++ Portfolio/src/components/AboutMe.jsx
@@ -1,7 +1,16 @@
-
+import miaImage from '../assets/IMG_5769.jpeg'
 
 export default function AboutMe () {
     return (
-        <h1>I be over here I be over there.</h1>
+        <div className= "about-me">
+            <div className= "about-me-img">
+                <img src= {miaImage} alt= "Casual Looking Mia" />
+            </div>
+            <div className= "about-me-des">
+                <h1>I be over here I be over there.</h1>
+            </div>
+
+        </div>
+
     )
 }
